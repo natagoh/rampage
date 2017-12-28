fc5e453c95191f035c1eb81db309c0d91c1351c5
natagoh <natagoh@stanford.edu>
2017-12-28 14:37:35 -0500

    removed react start stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // virtual bookshelf
@@ -34,18 +33,13 @@ class Book extends Component {
 }
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
 
         <Book title="The Name of the Wind" />
+
       </div>
     );
   }
