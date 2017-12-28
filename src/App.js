02d841860653ce97b35bccdac49678d2f8ff9d46
natagoh <natagoh@stanford.edu>
2017-12-28 16:25:33 -0500

    tracking window heights yass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav, FormGroup, FormControl, Button } from 'react-bootstrap';
 var Alert = require('react-bootstrap/lib/Alert');
 
 // virtual bookshelf
 class Bookshelf extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    //console.log('window dimensions', window.innerWidth)
     return (
-      <p>hi</p>
+      <p>hi this is a bookshelf</p>
     )
   }
 }
@@ -26,7 +33,7 @@ class Book extends Component {
 
   render() {
     return (
-      <div class="Book-tile">
+      <div className="Book-tile">
         <p> { this.props.title } </p>
         <p>testing</p>
       </div>
@@ -34,14 +41,108 @@ class Book extends Component {
   }
 }
 
-class App extends Component {
-  
+// keeps track of navbar height
+class SmartNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { nav_height: '0' };
+    this.updateNavDimensions = this.updateNavDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateNavDimensions();
+    window.addEventListener('resize', this.updateNavDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateNavDimensions);
+  }
+
+  updateNavDimensions() {
+    const height = document.getElementById('smart-nav').clientHeight;
+    this.setState({ nav_height: height });
+  }
+
   render() {
     return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top" id="smart-nav">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu" aria-labelledby="dropdown01">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { window_width: '0', window_height: '0' };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ window_width: window.innerWidth, window_height: window.innerHeight });
+  }
+
+
+  render() {
+  
+
+    const wellStyles = { maxWidth: 400, margin: '0 auto 10px' };
+
+const buttonsInstance = (
+  <div className="well" style={wellStyles}>
+    <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
+    <Button bsSize="large" block>Block level button</Button>
+  </div>
+);
+
+
+    return (
       <div>
+       
 
+        <SmartNav />
         <Book title="The Name of the Wind" />
-
+        <Bookshelf />
+        {buttonsInstance}
       </div>
     );
   }
